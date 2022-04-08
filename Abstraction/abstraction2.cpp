9714382e34aad4270109ac806ea8692c485902ea
kabirajk <kabiraj.mc19@bitsathy.ac.in>
2022-04-08 21:53:14 +0530

    Abstraction

---
+++ Abstraction/abstraction2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+class cred
+{  
+    public:
+    string username;
+    string password;
+    cred()
+    {
+        cout<<"enter the user name: ";
+        cin>>username;
+        cout<<"enter the password: ";
+        cin>>password;
+    }
+};
+class login
+{   private:
+    cred * login_list= (cred*) malloc(sizeof(cred)*3);
+    int last_id=0;
+    void auth_cred()
+    {
+        string un;
+        string pw;
+        cout<<"\nenter the username:";
+        cin>>un;
+        cout<<"\nenter the password:";
+        cin>>pw;
+        int flag=1;
+        for (int i=0;i<3;i++)
+        {
+            if(un==login_list[i].username)
+                if(pw==login_list[i].password)
+                    {cout<<"success";
+                    flag=1;
+                    break;}
+                else cout<<"invalid";
+            else flag=0;
+        }   
+        if (flag==0)
+            cout<<"invalid";
+    }
+    public:
+    void add_cred()
+    {   if (last_id==3)return;
+        login_list[last_id]=cred();
+        last_id+=1;
+    }
+    void Login()
+    {auth_cred();}
+};
+int main()
+{   
+    login l;
+    l.add_cred();
+    l.Login();
+    return 0;
+}
